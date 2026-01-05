414100dd408ae4f4f783aa67723f6cc7dcc71fd7
Cyril Plisko <cyril.plisko@mountall.com>
2026-01-05 15:16:47 +0200

    StatefulSetGetExt: more spec accessors

--- src/get/statefulset.rs
+++ src/get/statefulset.rs
@@ -6,6 +6,26 @@ pub trait StatefulSetGetExt {
     fn status(&self) -> Option<&appsv1::StatefulSetStatus>;
 
     // From spec
+    fn ordinals(&self) -> Option<i32> {
+        self.spec()?.ordinals.as_ref()?.start
+    }
+
+    fn pod_management_policy(&self) -> Option<&str> {
+        self.spec()?.pod_management_policy.as_deref()
+    }
+
+    fn revision_history_limit(&self) -> Option<i32> {
+        self.spec()?.revision_history_limit
+    }
+
+    fn service_name(&self) -> Option<&str> {
+        self.spec()?.service_name.as_deref()
+    }
+
+    fn spec_replicas(&self) -> Option<i32> {
+        self.spec()?.replicas
+    }
+
     fn min_ready_seconds(&self) -> Option<i32> {
         self.spec()?.min_ready_seconds
     }
@@ -14,32 +34,41 @@ pub trait StatefulSetGetExt {
     fn available_replicas(&self) -> Option<i32> {
         self.status()?.available_replicas
     }
+
     fn collision_count(&self) -> Option<i32> {
         self.status()?.collision_count
     }
+
     fn conditions(&self) -> Option<&[appsv1::StatefulSetCondition]> {
         self.status()?.conditions.as_deref()
     }
+
     fn current_replicas(&self) -> Option<i32> {
         self.status()?.current_replicas
     }
+
     fn current_revision(&self) -> Option<&str> {
         self.status()?.current_revision.as_deref()
     }
+
     fn observed_generation(&self) -> Option<i64> {
         self.status()?.observed_generation
     }
+
     fn ready_replicas(&self) -> Option<i32> {
         self.status()?.ready_replicas
     }
+
     fn status_replicas(&self) -> i32 {
         self.status()
             .map(|status| status.replicas)
             .unwrap_or_default()
     }
+
     fn update_revision(&self) -> Option<&str> {
         self.status()?.update_revision.as_deref()
     }
+
     fn updated_replicas(&self) -> Option<i32> {
         self.status()?.updated_replicas
     }
